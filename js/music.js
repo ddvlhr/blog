0f88d83b7a63baf3df39476a3ce251729fadfacc
ddvlhr <354874258@qq.com>
2022-12-18 21:54:40 +0800

    Site updated: 2022-12-18 21:54:38

--- js/music.js
+++ js/music.js
@@ -1,13 +1,15 @@
 const ap = new APlayer({
   container: document.getElementById("aplayer"),
+  volume: 0.5,
   fixed: true,
-  autoplay: true,
+  autoplay: false,
   audio: [
     {
-      name: "",
-      artist: "",
-      url: "",
-      cover: "",
+      name: "Gimme! Gimme! Gimme! (A Man After Midnight)",
+      artist: "ABBA",
+      url: " http://music.163.com/song/media/outer/url?id=3880252.mp3",
+      cover:
+        "http://p2.music.126.net/DGp1Z5eqqGit_L3hg7h48w==/109951166365655176.jpg?param=130y130",
     },
   ],
 });
